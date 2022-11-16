f2131da81f5810786049a36af7048ace72a55905
Yifan Kang <yifank@clemson.edu>
2022-11-16 09:54:03 +0900

    switch to d3 for loading data; might be easier for future dev

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,43 +1,42 @@
 var map = L.map('map').setView([40.7055025, -73.977681], 10);
 var geojson;
 var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
 var scaleColor = d3.scaleSequential(d3.interpolateRdYlGn)
-                  .domain(d3.extent(zipdata.features, d => +d.properties.AvgScore))
+  .domain(d3.extent(zipdata.features, d => +d.properties.AvgScore))
 
 
-$.get('assets/data/restrauntAvg.csv', function(csvString) {
-
-// Use PapaParse to convert string to array of objects
-var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
-
-// For each row in data, create a marker and add it to the map
-// For each row, columns `Latitude`, `Longitude`, and `Title` are required
-for (var i in data) {
-  var row = data[i];
-  console.log(row)
-
-  var marker = L.marker([row.Latitude, row.Longitude], {
-    opacity: 1
-  }).bindPopup(row.DBA);
+d3.csv('assets/data/restrauntAvg.csv').then(function (data) {
+  // For each row in data, create a marker and add it to the map
+  // For each row, columns `Latitude`, `Longitude`, and `Title` are required
+  data.forEach(function (d, i) {
   
-  marker.addTo(map);
-}
+  //log only the first 10 rows
+    if (i < 10) {
+      console.log(d);
+    }
+
+    var marker = L.marker([d.Latitude, d.Longitude], {
+      opacity: 1
+    }).bindPopup(d.DBA);
+
+    marker.addTo(map);
+  })
 
 });
 
 function style(feature) {
   return {
-      fillColor: scaleColor(feature.properties.AvgScore),
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 0.5
+    fillColor: scaleColor(feature.properties.AvgScore),
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.5
   };
 }
 
@@ -45,10 +44,10 @@ function highlightFeature(e) {
   var layer = e.target;
 
   layer.setStyle({
-      weight: 3,
-      color: '#666',
-      dashArray: '',
-      fillOpacity: 0.5
+    weight: 3,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.5
   });
 
   layer.bringToFront();
@@ -65,9 +64,9 @@ function zoomToFeature(e) {
 
 function onEachFeature(feature, layer) {
   layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-      click: zoomToFeature
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature
   });
 }
 
