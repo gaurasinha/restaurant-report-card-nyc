db7171ade3925b1bdb2dcb4d1cd76aacb31a50c9
mahfuz195 <asaroj@g.clemson.edu>
2022-11-23 00:42:20 -0500

    bar chart for violation stage 1

--- assets/js/map.js
+++ assets/js/map.js
@@ -47,7 +47,7 @@ var bars;
 var svgGroups;
 var markerLayer;
 
-var genCuisine = d3.select("#cuisines")
+ 
 
 var scaleColor = d3.scaleSequential(d3.interpolateRdYlGn)
   .domain(d3.extent(zipdata.features, d => d.properties.AvgScore > 0 ? -d.properties.AvgScore : -2))
@@ -60,6 +60,7 @@ function zipColor(d) {
 
 var restData;
 var gradeData;
+var ViolationCodeData;
 d3.csv('assets/data/restrauntAvg.csv').then(function (data) {
 
   restData = data;
@@ -68,15 +69,20 @@ d3.csv('assets/data/restrauntAvg.csv').then(function (data) {
     style: style,
     onEachFeature: onEachFeature
   }).addTo(map);
+  d3.csv('assets/data/ViolationData.csv').then(function(Vdata){
+    ViolationCodeData = Vdata
+      })
   d3.csv('assets/data/Restaurant_Grades_converted.csv').then(function(dataGrade){
-
     gradeData = dataGrade
-    setTimeout(showCuisine('11378'), 1000);
+    
+    setTimeout(showCuisine('11378'), 10000);
     map.flyToBounds([
       [40.73551917894102, -73.88564075991255],
       [40.71269707125203, -73.93131508939085]
   ]);
   })
+  
+
 });
 
 
@@ -395,8 +401,231 @@ function showCuisine(zip) {
   marker =L.marker([d.Latitude,d.Longitude], {
     opacity: 1,
     icon: markerIcon[Math.min(2, parseInt(d.AvgScore / 14))]
+
+    
+    
   }).bindPopup(popup)
   markerList.push(marker)
 })
 markerLayer = L.layerGroup(markerList).addTo(map)
+
+// violation data
+// filteredViolationData = restData.filter(d=> selectedZip.includes(d.ZIPCODE))
+  filteredData.forEach(function (d) {
+  VCData = ViolationCodeData.filter(function (e){return (d.Latitude==e.Latitude)})
+  })
+  console.log(VCData);
+
+  const sample = [
+    {
+      language: '10F',
+      value: 78.9,
+      color: '#000000'
+    },
+    {
+      language: '08A',
+      value: 75.1,
+      color: '#00a2ee'
+    },
+    {
+      language: '04L',
+      value: 68.0,
+      color: '#fbcb39'
+    },
+    {
+      language: '06D',
+      value: 67.0,
+      color: '#007bc8'
+    },
+    {
+      language: '06C',
+      value: 65.6,
+      color: '#65cedb'
+    },
+    {
+      language: '10B',
+      value: 65.1,
+      color: '#ff6e52'
+    },
+    {
+      language: '02G',
+      value: 61.9,
+      color: '#f9de3f'
+    },
+    {
+      language: '04N',
+      value: 60.4,
+      color: '#5d2f8e'
+    },
+    {
+      language: '02B',
+      value: 59.6,
+      color: '#008fc9'
+    },
+    {
+      language: '04M',
+      value: 59.6,
+      color: '#507dca'
+    }
+  ];
+
+  // const svg = d3.select('violationSvg');
+  var violationSvg = d3.select("#violation")
+  const svgContainer = d3.select('#container');
+  
+  const margin = 80;
+  const width = 1000 - 2 * margin;
+  const height = 600 - 2 * margin;
+
+  const chart = violationSvg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`);
+
+  const xScale = d3.scaleBand()
+    .range([0, width])
+    .domain(sample.map((s) => s.language))
+    .padding(0.4)
+  
+  const yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, 100]);
+
+  // vertical grid lines
+  // const makeXLines = () => d3.axisBottom()
+  //   .scale(xScale)
+
+  const makeYLines = () => d3.axisLeft()
+    .scale(yScale)
+
+  chart.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
+
+  chart.append('g')
+    .call(d3.axisLeft(yScale));
+
+  // vertical grid lines
+  // chart.append('g')
+  //   .attr('class', 'grid')
+  //   .attr('transform', `translate(0, ${height})`)
+  //   .call(makeXLines()
+  //     .tickSize(-height, 0, 0)
+  //     .tickFormat('')
+  //   )
+
+  chart.append('g')
+    .attr('class', 'grid')
+    .call(makeYLines()
+      .tickSize(-width, 0, 0)
+      .tickFormat('')
+    )
+
+  const barGroups = chart.selectAll()
+    .data(sample)
+    .enter()
+    .append('g')
+
+  barGroups
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (g) => xScale(g.language) )
+    .attr('y', (g) => yScale(g.value))
+    .attr('height', (g) => height - yScale(g.value))
+    .attr('width', xScale.bandwidth())
+    .on('mouseenter', function (actual, i) {
+      d3.selectAll('.value')
+        .attr('opacity', 0)
+
+      d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 0.6)
+        .attr('x', (a) => xScale(a.language) - 5)
+        .attr('width', xScale.bandwidth() + 10)
+        
+
+      const y = yScale(actual.value)
+
+      line = chart.append('line')
+        .attr('id', 'limit')
+        .attr('x1', 0)
+        .attr('y1', y)
+        .attr('x2', width)
+        .attr('y2', y)
+
+      barGroups.append('text')
+        .attr('class', 'divergence')
+        .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.value) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a, idx) => {
+          const divergence = (a.value - actual.value).toFixed(1)
+          
+          let text = ''
+          if (divergence > 0) text += '+'
+          text += `${divergence}%`
+
+          return idx !== i ? text : '';
+        })
+
+    })
+    .on('mouseleave', function () {
+      d3.selectAll('.value')
+        .attr('opacity', 1)
+
+      d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 1)
+        .attr('x', (a) => xScale(a.language))
+        .attr('width', xScale.bandwidth())
+
+      chart.selectAll('#limit').remove()
+      chart.selectAll('.divergence').remove()
+    })
+
+  barGroups 
+    .append('text')
+    .attr('class', 'value')
+    .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+    .attr('y', (a) => yScale(a.value) + 30)
+    .attr('text-anchor', 'middle')
+    .text((a) => `${a.value}%`)
+    .attr('fill', 'white')
+  
+    violationSvg
+    .append('text')
+    .attr('class', 'label')
+    .attr('x', -(height / 2) - margin)
+    .attr('y', margin / 2.4)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Count')
+    .attr('fill', 'white')
+
+    violationSvg.append('text')
+    .attr('class', 'label')
+    .attr('x', width / 2 + margin)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'middle')
+    .text('Violation Code')
+    .attr('fill', 'white')
+
+    violationSvg.append('text')
+    .attr('class', 'title')
+    .attr('x', width / 2 + margin)
+    .attr('y', 40)
+    .attr('text-anchor', 'middle')
+    .text('Violations in New York')
+    .attr('fill', 'white')
+
+    violationSvg.append('text')
+    .attr('class', 'source')
+    .attr('x', width - margin / 2)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'start')
+    .attr('fill', 'white')
+    
+
+
+
 }
