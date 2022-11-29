4f094ff87bc5e1a7384ba83a4bca88f74e04feef
mahfuz195 <asaroj@g.clemson.edu>
2022-11-29 08:15:43 -0500

    bar chart for violation

--- assets/js/map.js
+++ assets/js/map.js
@@ -420,8 +420,12 @@ markerLayer = L.layerGroup(markerList).addTo(map)
 violationsInSelectedArea();
 }
 
+
 function violationsInSelectedArea(){
-  // violation data
+  var svgarea = d3.select("violation");
+  svgarea.selectAll("*").remove();
+
+  // violation data 
 // filteredViolationData = restData.filter(d=> selectedZip.includes(d.ZIPCODE))
 filteredViolationData = ViolationCodeData.filter(d=> selectedZip.includes(d.ZIPCODE))
 // filteredData.forEach(function (d) {
@@ -445,163 +449,208 @@ myMap.forEach(function(value, key) {
 })
 
 console.log(arrayDatas);
+var maxvalue = arrayTmp[0][1];
 
 
-// const svg = d3.select('violationSvg');
+// const svgContainer = d3.select('#container');
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 660 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+
 var violationSvg = d3.select("#violation")
-const svgContainer = d3.select('#container');
+.append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-const margin = 80;
-const width = 1000 - 2 * margin;
-const height = 600 - 2 * margin;
+// Parse the Data
 
-const chart = violationSvg.append('g')
-  .attr('transform', `translate(${margin}, ${margin})`);
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(arrayDatas)
+    .padding(0.1);
+    violationSvg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .style("text-anchor", "end");
 
-const xScale = d3.scaleBand()
-  .range([0, width])
-  .domain(arrayDatas)
-  .padding(0.4)
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, maxvalue])
+    .range([ height, 0]);
+    violationSvg.append("g")
+    .call(d3.axisLeft(y));
 
-const yScale = d3.scaleLinear()
-  .range([height, 0])
-  .domain([0, 100]);
+  // Bars
+  violationSvg.selectAll("mybar")
+    .data(arrayTmp)
+    .enter()
+    .append("rect")
+      .attr("x", d => x(d[0]) )
+      .attr("y",d => y(d[1]) )
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d[1]); })
+      .attr("fill", "#69b3a2")
+      .padding(0.5)
 
-// vertical grid lines
-// const makeXLines = () => d3.axisBottom()
-//   .scale(xScale)
+// const margin = 80;
+// const width = 1000 - 2 * margin;
+// const height = 600 - 2 * margin;
 
-const makeYLines = () => d3.axisLeft()
-  .scale(yScale)
+// const chart = violationSvg.append('g')
+//   .attr('transform', `translate(${margin}, ${margin})`);
 
-chart.append('g')
-  .attr('transform', `translate(0, ${height})`)
-  .call(d3.axisBottom(xScale));
+// const xScale = d3.scaleBand()
+//   .range([0, width])
+//   .domain(arrayDatas)
+//   .padding(0.4)
 
-chart.append('g')
-  .call(d3.axisLeft(yScale));
+// const yScale = d3.scaleLinear()
+//   .range([height, 0])
+//   .domain([0, 100]);
 
-// vertical grid lines
+// // vertical grid lines
+// // const makeXLines = () => d3.axisBottom()
+// //   .scale(xScale)
+
+// const makeYLines = () => d3.axisLeft()
+//   .scale(yScale)
+
+// chart.append('g')
+//   .attr('transform', `translate(0, ${height})`)
+//   .call(d3.axisBottom(xScale));
+
+// chart.append('g')
+//   .call(d3.axisLeft(yScale));
+
+// // vertical grid lines
+// // chart.append('g')
+// //   .attr('class', 'grid')
+// //   .attr('transform', `translate(0, ${height})`)
+// //   .call(makeXLines()
+// //     .tickSize(-height, 0, 0)
+// //     .tickFormat('')
+// //   )
+
 // chart.append('g')
 //   .attr('class', 'grid')
-//   .attr('transform', `translate(0, ${height})`)
-//   .call(makeXLines()
-//     .tickSize(-height, 0, 0)
+//   .call(makeYLines()
+//     .tickSize(-width, 0, 0)
 //     .tickFormat('')
 //   )
 
-chart.append('g')
-  .attr('class', 'grid')
-  .call(makeYLines()
-    .tickSize(-width, 0, 0)
-    .tickFormat('')
-  )
+// const barGroups = chart.selectAll()
+//   .data(arrayTmp)
+//   .enter()
+//   .append('g')
 
-const barGroups = chart.selectAll()
-  .data(arrayTmp)
-  .enter()
-  .append('g')
+// barGroups
+//   .append('rect')
+//   .attr('class', 'bar')
+//   .attr('x', (g) => xScale(g.key) )
+//   .attr('y', (g) => yScale(g.value))
+//   .attr('height', (g) => height - yScale(g.value))
+//   .attr('width', xScale.bandwidth())
+//   .on('mouseenter', function (actual, i) {
+//     d3.selectAll('.value')
+//       .attr('opacity', 0)
 
-barGroups
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', d => xScale(d[0]) )
-  .attr('y', (g) => yScale(g.value))
-  .attr('height', (g) => height - yScale(g.value))
-  .attr('width', xScale.bandwidth())
-  .on('mouseenter', function (actual, i) {
-    d3.selectAll('.value')
-      .attr('opacity', 0)
-
-    d3.select(this)
-      .transition()
-      .duration(300)
-      .attr('opacity', 0.6)
-      .attr('x', (a) => xScale(a.Vcode) - 5)
-      .attr('width', xScale.bandwidth() + 10)
+//     d3.select(this)
+//       .transition()
+//       .duration(300)
+//       .attr('opacity', 0.6)
+//       .attr('x', (a) => xScale(a.Vcode) - 5)
+//       .attr('width', xScale.bandwidth() + 10)
       
 
-    const y = yScale(actual.value)
+//     const y = yScale(actual.value)
 
-    // line = chart.append('line')
-    //   .attr('id', 'limit')
-    //   .attr('x1', 0)
-    //   .attr('y1', y)
-    //   .attr('x2', width)
-    //   .attr('y2', y)
+//     // line = chart.append('line')
+//     //   .attr('id', 'limit')
+//     //   .attr('x1', 0)
+//     //   .attr('y1', y)
+//     //   .attr('x2', width)
+//     //   .attr('y2', y)
 
-    barGroups.append('text')
-      .attr('class', 'divergence')
-      .attr('x', (a) => xScale(a.Vcode) + xScale.bandwidth() / 2)
-      .attr('y', (a) => yScale(a.value) + 30)
-      .attr('text-anchor', 'middle')
-      // .text((a, idx) => {
-      //   const divergence = (a.value - actual.value).toFixed(1)
+//     barGroups.append('text')
+//       .attr('class', 'divergence')
+//       .attr('x', (a) => xScale(a.Vcode) + xScale.bandwidth() / 2)
+//       .attr('y', (a) => yScale(a.value) + 30)
+//       .attr('text-anchor', 'middle')
+//       // .text((a, idx) => {
+//       //   const divergence = (a.value - actual.value).toFixed(1)
         
-      //   let text = ''
-      //   if (divergence > 0) text += '+'
-      //   text += `${divergence}%`
+//       //   let text = ''
+//       //   if (divergence > 0) text += '+'
+//       //   text += `${divergence}%`
 
-      //   return idx !== i ? text : '';
-      // })
+//       //   return idx !== i ? text : '';
+//       // })
 
-  })
-  .on('mouseleave', function () {
-    d3.selectAll('.value')
-      .attr('opacity', 1)
+//   })
+//   .on('mouseleave', function () {
+//     d3.selectAll('.value')
+//       .attr('opacity', 1)
 
-    d3.select(this)
-      .transition()
-      .duration(300)
-      .attr('opacity', 1)
-      .attr('x', (a) => xScale(a.Vcode))
-      .attr('width', xScale.bandwidth())
+//     d3.select(this)
+//       .transition()
+//       .duration(300)
+//       .attr('opacity', 1)
+//       .attr('x', (a) => xScale(a.Vcode))
+//       .attr('width', xScale.bandwidth())
 
-    // chart.selectAll('#limit').remove()
-    chart.selectAll('.divergence').remove()
-  })
+//     // chart.selectAll('#limit').remove()
+//     chart.selectAll('.divergence').remove()
+//   })
 
-barGroups 
-  .append('text')
-  .attr('class', 'value')
-  .attr('x', (a) => xScale(a.Vcode) + xScale.bandwidth() / 2)
-  .attr('y', (a) => yScale(a.value) + 30)
-  .attr('text-anchor', 'middle')
-  .text((a) => `${a.value}%`)
-  .attr('fill', 'white')
+// barGroups 
+//   .append('text')
+//   .attr('class', 'value')
+//   .attr('x', (a) => xScale(a.Vcode) + xScale.bandwidth() / 2)
+//   .attr('y', (a) => yScale(a.value) + 30)
+//   .attr('text-anchor', 'middle')
+//   .text((a) => `${a.value}%`)
+//   .attr('fill', 'white')
 
-  violationSvg
-  .append('text')
-  .attr('class', 'label')
-  .attr('x', -(height / 2) - margin)
-  .attr('y', margin / 2.4)
-  .attr('transform', 'rotate(-90)')
-  .attr('text-anchor', 'middle')
-  .text('Count')
-  .attr('fill', 'white')
+//   violationSvg
+//   .append('text')
+//   .attr('class', 'label')
+//   .attr('x', -(height / 2) - margin)
+//   .attr('y', margin / 2.4)
+//   .attr('transform', 'rotate(-90)')
+//   .attr('text-anchor', 'middle')
+//   .text('Count')
+//   .attr('fill', 'white')
 
-  violationSvg.append('text')
-  .attr('class', 'label')
-  .attr('x', width / 2 + margin)
-  .attr('y', height + margin * 1.7)
-  .attr('text-anchor', 'middle')
-  .text('Violation Code')
-  .attr('fill', 'white')
+//   violationSvg.append('text')
+//   .attr('class', 'label')
+//   .attr('x', width / 2 + margin)
+//   .attr('y', height + margin * 1.7)
+//   .attr('text-anchor', 'middle')
+//   .text('Violation Code')
+//   .attr('fill', 'white')
 
-  violationSvg.append('text')
-  .attr('class', 'title')
-  .attr('x', width / 2 + margin)
-  .attr('y', 40)
-  .attr('text-anchor', 'middle')
-  .text('Violations in New York')
-  .attr('fill', 'white')
+//   violationSvg.append('text')
+//   .attr('class', 'title')
+//   .attr('x', width / 2 + margin)
+//   .attr('y', 40)
+//   .attr('text-anchor', 'middle')
+//   .text('Violations in New York')
+//   .attr('fill', 'white')
 
-  violationSvg.append('text')
-  .attr('class', 'source')
-  .attr('x', width - margin / 2)
-  .attr('y', height + margin * 1.7)
-  .attr('text-anchor', 'start')
-  .attr('fill', 'white')
+//   violationSvg.append('text')
+//   .attr('class', 'source')
+//   .attr('x', width - margin / 2)
+//   .attr('y', height + margin * 1.7)
+//   .attr('text-anchor', 'start')
+//   .attr('fill', 'white')
   
 
 }
