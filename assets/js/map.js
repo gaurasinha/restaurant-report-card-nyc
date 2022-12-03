f8a433b938038a18d5b27d369b85068826154dbf
sarojarchana <asaroj@g.clemson.edu>
2022-12-03 17:22:37 -0500

    tootip after updating barchart

--- assets/js/map.js
+++ assets/js/map.js
@@ -821,6 +821,17 @@ function UpdatedviolationsInSelectedArea() {
   violationSvg.append("g")
     .call(d3.axisLeft(y));
 
+    tooltip = d3
+        .select('body')
+        .append('div')
+        .attr('class', 'd3-tooltip')
+        .style('position', 'absolute')
+        .style('z-index', '10')
+        .style('visibility', 'hidden')
+        .style('padding', '10px')
+        .style('background', 'rgba(0,0,0,0.6)')
+        .style('border-radius', '4px')
+        .style('color', '#fff')
   // Bars
   violationSvg.selectAll("mybar")
     .data(UarrayTmp)
@@ -830,7 +841,31 @@ function UpdatedviolationsInSelectedArea() {
     .attr("y", d => y(d[1]))
     .attr("width", x.bandwidth())
     .attr("height", function (d) { return height - y(d[1]); })
-    .attr("fill", "#69b3a2")
+        .attr("fill", "#69b3a2")
+      .on("mouseover", function (event, d) {
+          console.log(d[0])
+          console.log(a[d[0]])
+          tooltip
+              .html(
+                  (`<div>${a[d[0]]}</div>`)
+              )
+              .style('visibility', 'visible');
+          d3.select(this).transition().attr('fill', '#eec42d');
+
+
+      })
+      .on('mousemove', function (event, d) {
+          tooltip
+              .style('top', event.pageY - 10 + 'px')
+              .style('left', event.pageX + 10 + 'px');
+      })
+      .on("mouseout", function (event, d) {
+
+          tooltip.html(``).style('visibility', 'hidden');
+          d3.select(this).transition().attr('fill', "#69b3a2");
+
+
+      });
 }
 
 
