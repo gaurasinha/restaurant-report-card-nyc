9c962e60c518faafaa9e07b8fde2048af4168dbf
Yifan Kang <yifank@clemson.edu>
2022-12-06 06:02:48 +0900

    fix cuisine bar onClick lost bug

--- assets/js/map.js
+++ assets/js/map.js
@@ -279,11 +279,12 @@ function updateCuisine() {
     .attr('x', d => cuisineDimensions.margin.left)
     .attr("fill", d => groupColors[d[1]])
     .attr("height", yScaleCBar.bandwidth())
-      .attr("width", d => xScaleCBar(d[2]))
-      .attr('stroke', '#444')
-        .attr('stroke-width', d => (selectedCuisine === d[0] && d[1] === 2) * 4)
-      .style('paint-order', 'stroke')
-      .style('cursor', 'pointer')
+    .attr("width", d => xScaleCBar(d[2]))
+    .attr('stroke', '#444')
+    .attr('stroke-width', d => (selectedCuisine === d[0] && d[1] === 2) * 4)
+    .style('paint-order', 'stroke')
+    .style('cursor', 'pointer')
+    svgcuisine.select('g').selectAll('rect').on('click', clickCuisineBar)
   cuisineCountLabels.enter().append("text")
     .merge(cuisineCountLabels).transition()
     .duration(700)
@@ -851,20 +852,20 @@ function UpdatedviolationsInSelectedArea() {
       .attr('font-family', "sans-serif")
       .text("Violation Code");
 
-   
-      tooltip = d3
-          .select('body')
-          .append('div')
-          .attr('class', 'd3-tooltip')
-          .style('position', 'absolute')
-          .style('z-index', '10')
-          .style('visibility', 'hidden')
-          .style('padding', '10px')
-          .style('background', 'rgba(0,0,0,0.6)')
-          .style('border-radius', '4px')
-          .style('color', '#fff')
-          .style('width', violationTooltipWidth + 'px')
-          .style('font-size', 'small')
+
+    tooltip = d3
+      .select('body')
+      .append('div')
+      .attr('class', 'd3-tooltip')
+      .style('position', 'absolute')
+      .style('z-index', '10')
+      .style('visibility', 'hidden')
+      .style('padding', '10px')
+      .style('background', 'rgba(0,0,0,0.6)')
+      .style('border-radius', '4px')
+      .style('color', '#fff')
+      .style('width', violationTooltipWidth + 'px')
+      .style('font-size', 'small')
     // Bars
     violationSvg.selectAll("mybar")
       .data(UarrayTmp)
@@ -885,15 +886,15 @@ function UpdatedviolationsInSelectedArea() {
           .style('visibility', 'visible');
         d3.select(this).transition().attr('fill', '#29453e');
       })
-        .on('mousemove', function (event, d) {
-            tooltip
-                .style('top', event.pageY + 16 + 'px')
-                .style('left', function () {
-                    if (document.body.offsetWidth - event.pageX - 10 < violationTooltipWidth)
-                        return event.pageX - violationTooltipWidth + 'px';
-                    return event.pageX + 10 + 'px'
-                });
-        })
+      .on('mousemove', function (event, d) {
+        tooltip
+          .style('top', event.pageY + 16 + 'px')
+          .style('left', function () {
+            if (document.body.offsetWidth - event.pageX - 10 < violationTooltipWidth)
+              return event.pageX - violationTooltipWidth + 'px';
+            return event.pageX + 10 + 'px'
+          });
+      })
       .on("mouseout", function (event, d) {
         tooltip.html(``).style('visibility', 'hidden');
         d3.select(this).transition().attr('fill', "#69b3a2");
